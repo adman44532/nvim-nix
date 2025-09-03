bb94de72f536d269ec6aa409280c21c919fa0eee
adman44532 <adman44532@gmail.com>
2025-09-03 11:33:52 +1000

    fix: update render-markdown to show on avante

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,10 +1,14 @@
 return {
-  'render-markdown.nvim',
-  before = function ()
-    require('lz.n').trigger_load('nvim-treesitter')
-    require('lz.n').trigger_load('nvim-web-devicons')
+  "render-markdown.nvim",
+  before = function()
+    require("lz.n").trigger_load("nvim-treesitter")
+    require("lz.n").trigger_load("nvim-web-devicons")
   end,
-  after = function ()
-    require('render-markdown').setup({})
-  end
+  after = function()
+    require("render-markdown").setup({
+      completions = { blink = { enabled = true } },
+      file_types = { "markdown", "vimwiki", "Avante" },
+    })
+  end,
+  ft = { "markdown", "vimwiki", "Avante" },
 }
